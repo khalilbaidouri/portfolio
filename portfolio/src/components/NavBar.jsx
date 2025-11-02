1b55b22bfbb9e991fb5b970cb7cea6e2399f234a
hp <khalilbaidouri97@gmail.com>
2025-11-03 00:10:06 +0100

    tel

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -12,21 +12,25 @@ const NavBar = () => {
             <span className="text-teal-800 font-orbitron">KHALIL-BAIDOURI</span>
           </a>
 
-          <div className="hidden md:flex space-x-8 border border-gray-200 rounded-full px-10 py-4">
+          <div className="hidden md:flex space-x-8 border border-gray-200 rounded-full px-10 py-4 w-">
             {navMenu.map((item, index) => (
-              <a
-                key={index}
-                href={`#${item.toLowerCase()}`}
-                className="text-gray-700 hover:text-teal-700 transition duration-300"
-              >
-                {item}
-              </a>
+              <div className="">
+                <a
+                  key={index}
+                  href={`#${item.toLowerCase()}`}
+                  className="text-gray-700 hover:text-teal-700 transition duration-300"
+                >
+                  {item}
+                </a>
+              </div>
             ))}
           </div>
 
-          <div>
-            <a download href="/CV.pdf"
-              className="px-6 py-3 border border-zinc-800 rounded-full flex items-center gap-2 text-slate-600 hover:text-slate-900 hover:border-teal-700 hover:translate-y-1 transition duration-300 cursor-pointer"
+          <div className="flex flex-col md:flex-row">
+            <a
+              download
+              href="/CV.pdf"
+              className="px-6 py-3 border border-zinc-800 rounded-full flex items-center gap-2 text-slate-600 hover:text-slate-900 hover:border-teal-700 hover:translate-y-1 transition duration-300 cursor-pointer "
             >
               Mon CV
               <FaArrowRight className="text-gray-500 text-sm" />
